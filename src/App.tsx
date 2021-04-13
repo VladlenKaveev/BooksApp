dbcab1f0147e40cf9ffb4152b28ec191f97bed53
vladlenkaveev <vlad737290@gmail.com>
2021-04-13 18:01:31 +0300

    Change Boot Splash Screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import Navigation from './modules/navigation';
 import store from './store';
+import RNBootSplash from 'react-native-bootsplash';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    RNBootSplash.hide({fade: true});
+  }
   render() {
     return (
       <Provider store={store}>
